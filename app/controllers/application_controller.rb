244e2aff1b2b48ffce2676d6887b87e275c7bbfb
rinoale <rinoale@hanmail.net>
2016-08-08 07:37:54 +0000

    remove mock menus

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,18 +9,17 @@ class ApplicationController < ActionController::Base
 
   def left_menu_node
     @left_menu_node = [
-      { menu_name: 'GuildInfo', link: guilds_path },
+      { menu_name: 'Guilds', link: guilds_path },
       { menu_name: 'Parties', link: guilds_path }
     ]
   end
 
   def upper_menu_node
     @upper_menu_node = [
-      { menu_name: 'One', link: root_path },
-      { menu_name: 'Two', link: root_path },
-      { menu_name: 'Three', link: root_path },
-      { menu_name: 'Four', link: root_path },
-      { menu_name: 'Five', link: root_path }
+      { menu_name: 'Introduce', link: root_path },
+      { menu_name: 'FreeBoard', link: root_path },
+      { menu_name: 'Gallery', link: root_path },
+      { menu_name: 'QnA', link: root_path }
     ]
   end
 end
