c050a6925b045a0cf34f599825ae0101e324b84b
rinoale <rinoale@hanmail.net>
2016-08-09 08:54:55 +0000

    new, index and form page

--- app/controllers/guilds/guild_boards_controller.rb
+++ app/controllers/guilds/guild_boards_controller.rb
@@ -1,51 +1,54 @@
 module Guilds
   class GuildBoardsController < ApplicationController
+    helper_method :guild
+
     def index
-      @boards = guild.guild_board
+      @guild_boards = guild.guild_board
     end
-    #
-    # def show
-    #   @guild = Guild.find_by_guild_address(params[:guild_address])
-    #
-    #   @left_menu_node = [
-    #     { menu_name: @guild.guild_name, link: guild_path(@guild) },
-    #     { menu_name: 'Parties', link: guild_path(@guild) },
-    #     { menu_name: 'Votes', link: guild_path(@guild) },
-    #     { menu_name: 'Guild BBS', link: guild_path(@guild) }
-    #   ]
-    # end
-    #
+
+    def show
+      @guild_board = guild.guild_board.find(params[:id])
+
+      @left_menu_node = [
+        { menu_name: @guild.guild_name, link: guild_path(@guild) },
+        { menu_name: 'Parties', link: guild_path(@guild) },
+        { menu_name: 'Votes', link: guild_path(@guild) },
+        { menu_name: 'Guild BBS', link: guild_path(@guild) }
+      ]
+    end
+
     def new
-      @board = guild.guild_board.new
+      @guild_board = guild.guild_board.new
     end
 
     def edit
-      @board = guild.guild_board.find(params[:id])
+      @guild_board = guild.guild_board.find(params[:id])
     end
 
     def create
-      @board = guild.guild_board.new(board_params)
+      @guild_board = guild.guild_board.new(board_params)
+      debugger
 
-      if @board.save
-        redirect_to @board
+      if @guild_board.save
+        redirect_to guild_guild_board_url(guiid, @guild_board)
       else
         render 'new'
       end
     end
 
     def update
-      @board = guild.guild_board.find(params[:id])
+      @guild_board = guild.guild_board.find(params[:id])
 
-      if @board.update(board_params)
-        redirect_to @board
+      if @guild_board.update(board_params)
+        redirect_to @guild_board
       else
         render 'edit'
       end
     end
 
     def destroy
-      @board = guild.guild_board.find(params[:id])
-      @board.destroy
+      @guild_board = guild.guild_board.find(params[:id])
+      @guild_board.destroy
 
       redirect_to guild_guild_boards_path
     end
@@ -53,7 +56,7 @@ module Guilds
     private
 
     def board_params
-      params.require(:board).permit(:title, :content, :author)
+      params.require(:guild_board).permit(:title, :content, :author)
     end
 
     def left_menu_node
