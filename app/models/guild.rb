88548ccaf0bb5659e05a35bb29943543be95541c
rinoale <rinoale@hanmail.net>
2016-08-12 04:05:56 +0000

    only lowercase letters for guild address

--- app/models/guild.rb
+++ app/models/guild.rb
@@ -2,6 +2,7 @@ class Guild < ActiveRecord::Base
   has_many :guild_board
 
   validates :guild_address, :guild_name, :guild_master, presence: { message: 'can not be empty' }
+  validates :guild_address, format: { with: /\A[a-z]+\z/, message: 'can have only lowercase letters' }
 
   def to_param
     guild_address
