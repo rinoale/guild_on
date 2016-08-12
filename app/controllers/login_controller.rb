c69085bcc1808d448786419ebf214712a594ac7b
rinoale <rinoale@hanmail.net>
2016-08-12 08:49:47 +0000

    fill create and destroy action on login controller

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -3,8 +3,18 @@ class LoginController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:emailid])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
   end
 end
