3dcc4c631edad20e6f3650c0775f1838e5b3e481
rinoale <rinoale@hanmail.net>
2016-08-13 14:29:37 +0000

    commit methods in user helper but codes are secret

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+  def send_auth_code username
+  end
+
+  def encode_username username
+    if username.match /[[:alpha]]/
+      username
+    else
+      buffer_array = username.encode(Encoding::EUC_KR).unpack 'H2' * (username.length * 2)
+      kor_name = buffer_array.map do |value| '%' + value.upcase end.join
+    end
+  end
 end
